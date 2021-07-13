e6ef8c0071734c0b78dfe4e3690a2cbcf8192931
anyuru <davidderrickanyuru@gmail.com>
2021-07-13 11:41:06 +0300

    Destructuring UseCases in javaScript

--- calculate.js
+++ calculate.js
@@ -6,8 +6,25 @@ let sum = (num1, num2) => {
     return num1 + num2 
 }
 
+
+let subtract = (num1, num2) => {
+    if(typeof num1 !== 'number') return false
+    if(typeof num2 !== 'number') return false
+   
+    return num1 - num2  
+}
+
 // console.log(sum(5, 9))
 // console.log(sum(o, 9)) //This can be very hard to troubleshoot because the system compiler won't return an informative error message 
-// console.log(sum(10, '11'))
+// console.log(sum(10, '11')
 
-module.exports = sum
+
+// Export one fuction
+// module.exports = sum
+
+//Export more than one function
+module.exports = {
+    sum, 
+    subtract
+}
+
